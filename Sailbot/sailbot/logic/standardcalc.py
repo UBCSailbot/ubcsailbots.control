5dae45803d2d58c79e75e866310e5f820d4b6b8b
Michael Peterson <peterson_michael_j@hotmail.com>
2013-01-26 14:44:58 -0800

    Added more structure to point to point and implemented function to find distance between two points

--- Sailbot/sailbot/logic/standardcalc.py
+++ Sailbot/sailbot/logic/standardcalc.py
@@ -3,9 +3,17 @@ Created on Jan 20, 2013
 
 @author: joshandrews
 '''
+import math
 
-def distBetweenTwoPoints(point1, point2):
-    return 0
+def distBetweenTwoCoords(coord1, coord2):
+    dLongRad = math.radians(coord1.long - coord2.long)
+    dLatRad = math.radians(coord1.lat - coord2.lat)
+    latRad1 = math.radians(coord1.lat)
+    latRad2 = math.radians(coord2.lat)
+    
+    a = math.pow(math.sin(dLatRad/2),2) + math.cos(latRad1)*math.cos(latRad2)*math.pow(math.sin(dLongRad/2),2)
+    c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+    return 6371.0*c #6371 is mean radius of earth in km
 
 def angleBetweenTwoHeadings(heading1, heading2):
     return 0
