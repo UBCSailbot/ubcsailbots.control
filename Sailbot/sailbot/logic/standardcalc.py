2771f1c3af15d27b24660f32340ed0c3f3e7f892
Michael Peterson <peterson_michael_j@hotmail.com>
2013-01-26 17:38:25 -0800

    Added and implemented distBetweenTwoCoords and angleBetweenTwoCoords helper functions

--- Sailbot/sailbot/logic/standardcalc.py
+++ Sailbot/sailbot/logic/standardcalc.py
@@ -4,6 +4,7 @@ Created on Jan 20, 2013
 @author: joshandrews
 '''
 import math
+from sailbot.datatype import datatypes
 
 def distBetweenTwoCoords(coord1, coord2):
     dLongRad = math.radians(coord1.long - coord2.long)
@@ -15,5 +16,48 @@ def distBetweenTwoCoords(coord1, coord2):
     c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
     return 6371.0*c #6371 is mean radius of earth in km
 
-def angleBetweenTwoHeadings(heading1, heading2):
-    return 0
+def angleBetweenTwoCoords(sourceCoord, destCoord):
+    GPSCoord = datatypes.GPSCoordinate
+    
+    if(sourceCoord.lat > destCoord.lat):
+        GPSCoord.lat = sourceCoord.lat
+        GPSCoord.long = destCoord.long
+    
+    elif(sourceCoord.lat < destCoord.lat):
+        GPSCoord.lat = destCoord.lat
+        GPSCoord.long = sourceCoord.long
+    
+    elif(sourceCoord.long < destCoord.long):
+        return -90
+    
+    elif(sourceCoord.long > destCoord.long):
+        return 90
+    
+    else:
+        return None
+    
+    
+    distBtwnCoords = distBetweenTwoCoords(sourceCoord, destCoord)
+    distSin = distBetweenTwoCoords(destCoord, GPSCoord)
+    
+    angle = math.asin(distSin/distBtwnCoords)
+    
+    if(sourceCoord.lat < destCoord.lat):
+        if(sourceCoord.long < destCoord.long):
+            angle = -angle
+            return datatypes.Angle(angle)
+        elif(sourceCoord.long > destCoord.long):
+            return datatypes.Angle(angle)
+        else:
+            return datatypes.Angle(0)        
+    else:
+        if(sourceCoord.long < destCoord.long):
+            angle = -90-angle
+            return datatypes.Angle(angle)
+        elif(sourceCoord.long > destCoord.long):
+            angle = 90+angle
+            return datatypes.Angle(angle)
+        else:
+            return datatypes.Angle(180)
+            
+            
