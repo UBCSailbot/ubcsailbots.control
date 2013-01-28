6fec56b3f0cf4b81bdd9dbda88d9e951b7182c1c
Michael Peterson <peterson_michael_j@hotmail.com>
2013-01-27 17:30:27 -0800

    Added angle test and fixed dist test

--- Sailbot/sailbot/tests/test_standardcalc.py
+++ Sailbot/sailbot/tests/test_standardcalc.py
@@ -9,5 +9,21 @@ class TestDistBetweenTwoCoords(unittest.TestCase):
         self.point1 = datatypes.GPSCoordinate(0,0)
         self.point2 = datatypes.GPSCoordinate(1,1)
         
-    def test1(self):
-        self.assertEqual(round(standardcalc.distBetweenTwoCoords(self.point1, self.point2),1), 157.2)
+    def distTest1(self):
+        distance = standardcalc.distBetweenTwoCoords(self.point1, self.point2)
+        self.assertEqual(round(distance/1000,1), 157.2)
+        
+'''class TestAngleBetweenTwoCoords(unittest.TestCase):
+    def setUp(self):
+        self.source = datatypes.GPSCoordinate(100,100)
+        self.dest = datatypes.GPSCoordinate(300,100)
+        
+    def angleTest1(self):
+        angle1 = standardcalc.angleBetweenTwoCoords(self.source, self.dest)
+        angle2 = standardcalc.angleBetweenTwoCoords(self.dest, self.source)
+        
+        angle1value = self.angle1.degrees()
+        angle2value = self.angle2.degrees()
+        
+        self.assertEqual(angle1value, 0)
+        self.assertEqual(angle2value, 180)'''
