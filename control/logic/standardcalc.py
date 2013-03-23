3b7e5bcacc84ae5c56744c82860f2be6303986b3
nubreed141 <peterson_michael_j@hotmail.com>
2013-03-23 13:07:35 -0700

    Added a helper function to set a start point for roundBuoy

--- control/logic/standardcalc.py
+++ control/logic/standardcalc.py
@@ -174,5 +174,9 @@ def searchSOGIndex(numberSOG, list1SOG):
 def findCosLawAngle(a, b, c):  #cos law: c^2 = a^2 + b^2 - 2*a*b*cos(theta):
     return math.acos((math.pow(a, 2) + math.pow(b, 2) - math.pow(c, 2)) / (2*a*b))
         
+def setNavigatonBuoyPoint(buoyLocation, boatCoords, distFromBuoy):
+    interpoAngle = 90 - angleBetweenTwoCoords(buoyLocation, boatCoords)
+    xDelta = distFromBuoy*math.cos(interpoAngle)
+    yDelta = distFromBuoy*math.sin(interpoAngle)
     
-                    
+    return GPSDistAway(buoyLocation, xDelta, yDelta)              
